ab5b162578ee180429eba003a7f7f77ad398016c
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2025-03-03 04:00:36 +0000

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,22 +4,26 @@ import App from "./App";
 import { render } from "./test-utils";
 
 describe("App Component", () => {
+  // Checking if the main app renders without crashing
   test("renders without crashing", () => {
     render(<App />);
   });
 
+  // Check header title
   test("renders header with title", () => {
     const { getByText } = render(<App />);
     const title = getByText("My Good Reads");
     expect(title).toBeInTheDocument();
   });
 
+  // Check BookSearch component
   test("renders BookSearch component", () => {
     const { getByTestId } = render(<App />);
     const bookSearch = getByTestId("book-search");
     expect(bookSearch).toBeInTheDocument();
   });
 
+  // Check mobile wishlist
   test("renders WishlistMobile component", () => {
     const { getByTestId } = render(<App />);
     const wishlistMobile = getByTestId("wishlist-mobile-container");
