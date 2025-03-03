ac0ebf73e664ed94c3227fa669342abcf30fa337
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2025-03-03 11:59:52 +0000

    remove commented out old fetch method

--- src/book-search/book-search.tsx
+++ src/book-search/book-search.tsx
@@ -5,68 +5,6 @@ import { Book, BooksResponse } from "../types/books.types";
 import Pagination from "./pagination";
 import Wishlist from "../wishlist/wishlist";
 
-//     const [bookType, updateBookType] = useState("");
-//     const [bookTypeToSearch, updateBookTypeToSearch] = useState("");
-//     const [allAvailableBooks, setAllAvailableBooks] = useState([]);
-//     async function requestBooks() {
-//         if (bookTypeToSearch) {
-//             const allBooks = await getBooksByType(bookTypeToSearch);
-//             setAllAvailableBooks(allBooks);
-//         }
-//     }
-
-//     useEffect(() => {
-//         async function getAllBooks() {
-//             await requestBooks();
-//         }
-//         getAllBooks();
-//     }, [bookTypeToSearch]);
-//     return (
-//             <>
-//                 <div className="book--container">
-//                     <div className="search-params">
-//                         <div>
-//                             <form
-//                                 onSubmit={(e) => {
-//                                     debugger;
-//                                     e.preventDefault();
-//                                    updateBookTypeToSearch(bookType)
-//                                 }}
-//                             >
-//                                 <input
-//                                     className="full-width"
-//                                     autoFocus
-//                                     name="gsearch"
-//                                     type="search"
-//                                     value={bookType}
-//                                     placeholder="Search for books to add to your reading list and press Enter"
-//                                     onChange={e => updateBookType(e.target.value)}
-//                                 />
-//                             </form>
-//                             {!bookType && (
-//                                 <div className="empty">
-//                                     <p>
-//                                         Try searching for a topic, for example
-//                                         <a onClick={() => {
-//                                                 updateBookType("Javascript");
-//                                             }}
-//                                         >
-//                                             {" "}
-//                                             "Javascript"
-//                                         </a>
-//                                     </p>
-//                                 </div>
-//                             )}
-
-//                         </div>
-//                     </div>
-//                 </div>
-//                 {                <pre>{JSON.stringify(allAvailableBooks, null, 4)}</pre>
-//                 }
-//             </>
-//     );
-// };
-
 const BookSearch = () => {
   // State for managing books, loading state, search query, and pagination
   const [books, setBooks] = useState<Book[] | null>([]);
