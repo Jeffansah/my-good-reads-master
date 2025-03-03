30c58715053b97ff2ad0948b98db5fd26e248d41
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2025-03-03 04:46:59 +0000

    Update test-utils.tsx

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -2,15 +2,19 @@ import React, { PropsWithChildren } from "react";
 import { render, RenderOptions } from "@testing-library/react";
 import { WishlistProvider } from "./context/wishlist-context";
 
+// Wrapper component that provides necessary context providers
 const Providers = ({ children }: PropsWithChildren<{}>) => {
   return <WishlistProvider>{children}</WishlistProvider>;
 };
 
+// Custom render function that wraps components with necessary providers
 const customRender = (
   ui: React.ReactElement,
   options?: Omit<RenderOptions, "wrapper">
 ) => render(ui, { wrapper: Providers, ...options });
 
+// Re-export all testing library utilities
 export * from "@testing-library/react";
 
+// Export custom render as default render function
 export { customRender as render };
