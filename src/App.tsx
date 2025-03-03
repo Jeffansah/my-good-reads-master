786ba7f8df1e6ad86ac7700bc6cce71719ffbf58
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2025-03-03 04:23:28 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,22 @@ import BookSearch from "./book-search/book-search";
 import { WishlistProvider } from "./context/wishlist-context";
 import WishlistMobile from "./wishlist/wishlist-mobile";
 
+// Main application component
 function App() {
   return (
+    // Wrapper with wishlist context provider
     <WishlistProvider>
       <div className="app-section">
+        {/* Application header */}
         <header className="header">
           <div className="header--content">
             <h1>My Good Reads</h1>
           </div>
         </header>
 
+        {/* Main content area */}
         <BookSearch />
+        {/* Mobile wishlist panel */}
         <WishlistMobile />
       </div>
     </WishlistProvider>
